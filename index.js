a32b4405fd9ed29d7b22d5ee389173c8300ac623
srageh <ragehsuhaib@gmail.com>
2021-06-22 22:11:53 -0400

    finished form pages

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require("express");
 
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const app = express();
 
 const path = require("path");
 
-const routes = require("./routes/index");
-//const mysqlConnection = require("./config/dbconfig");
+const routes = require("./routes");
+//c
+const db = require("./config/dbconfig");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "./static")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/static")));
 
-app.use("/", routes);
+app.use("/", routes(db));
 
 app.listen(process.env.PORT, () => {
   //console.log(process.env);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,72 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
+module.exports = (db) => {
+  router.get("/", (req, res, next) => {
+    db.query("SELECT * FROM customer;", (err, result) => {
+      if (err) {
+        console.log("Error selecting customer " + err);
+      } else {
+        return res.render("index", { result });
+      }
+      return next(err);
+    });
+  });
 
-module.exports = router;
+  router.get("/update/:id", (req, res, next) => {
+    // const { id } = req.body;
+    // console.log("In update");
+    console.log(req.params.id);
+    const id = req.params.id;
+    //console.log(req.params.id);
+
+    //return next();
+
+    db.query("SELECT * FROM customer WHERE id=?", [id], (err, result) => {
+      if (err) {
+        console.log("Error selecting roq");
+      } else {
+        console.log(result);
+        return res.render("date", { result });
+      }
+      //return next();
+    });
+  });
+
+  router.post("/update/:id", (req, res, next) => {
+    console.log(req.body);
+    const { id, lastName, firstName, age, job, email } = req.body;
+    const sql =
+      "UPDATE customer SET lastName=?, firstName=?, age=?, job=?, email=? WHERE id =?";
+    db.query(sql, [lastName, firstName, age, job, email, id], (err, result) => {
+      if (err) {
+        console.log("Error updating ");
+      } else {
+        console.log(result);
+        res.send({ message: "sucess" });
+      }
+    });
+  });
+
+  router.get("/add", (req, res) => {
+    console.log("in add page");
+    res.render("add");
+  });
+
+  router.post("/add", (req, res, next) => {
+    console.log(req.body.lastName);
+    const { lastName, firstName, age, job, email } = req.body;
+    const sql =
+      "INSERT INTO customer (lastName,firstName,age,job,email) VALUES(?,?,?,?,?);";
+
+    db.query(sql, [lastName, firstName, age, job, email], (err, result) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  });
+
+  return router;
+};
